0a9179cb3887c42ff39162f5b4f5d104ac0b3536
Danil4615 <cerkashenkodanil@gmail.com>
2025-02-06 22:54:15 +0600

    Added function wich changes icons when user typing password and gives possibility to hide/unhide it

--- scripts/script.js
+++ scripts/script.js
@@ -22,3 +22,51 @@ registerButton.addEventListener("click", () => {
     loginForm.style.opacity = 0;
     registerForm.style.opacity = 1;
 })
+
+// View Password Functions
+
+const loginInputField = document.getElementById("loginPassword");
+const loginInputIcon = document.getElementById("login-password-icon");
+
+const registerInputField = document.getElementById("registerPassword");
+const registerInputIcon = document.getElementById("register-password-icon");
+
+function myLoginPassword() {
+    if(loginInputField.type === "password") {
+        loginInputField.type = "text";
+
+        loginInputIcon.name = "eye-off-outline";
+        loginInputIcon.style.cursor = "pointer";
+    } else {
+        loginInputField.type = "password";
+
+        loginInputIcon.name = "eye-outline";
+        loginInputIcon.style.cursor = "pointer";
+    }
+}
+
+function myRegisterPassword() {
+    if(registerInputField.type === "password") {
+        registerInputField.type = "text";
+
+        registerInputIcon.name = "eye-off-outline";
+        registerInputIcon.style.cursor = "pointer";
+    } else {
+        registerInputField.type = "password";
+
+        registerInputIcon.name = "eye-outline";
+        registerInputIcon.style.cursor = "pointer";
+    }
+}
+
+// Chanhe icon
+
+function changeIcon(value) {
+    if(value.length > 0) {
+        loginInputIcon.name = "eye-outline";
+        registerInputIcon.name = "eye-outline";
+    } else {
+        loginInputIcon.name = "lock-closed-outline";
+        registerInputIcon.name = "lock-closed-outline";
+    }
+}
